0cc2de6c21234b5a73dc98842b2091a1c44ecbe4
Mateus Assis <mateus-assis@outlook.com>
2021-07-20 11:23:37 -0300

    apagor logo e tudo pra nao ter perigo de dar merda

--- GraphicImageProcessing/test.py
+++
@@ -1,9 +0,0 @@
-
-from gip import CreateBoxes as CB 
-
-
-imagens = CB("../../imagem/_tuberculosis-phone-0088.jpg")
-
-imagens.ImageBox("object","bndbox","../../imagem/tuberculosis-phone-0088.xml","xml")
-
-imagens.MaskBox("object","bndbox","../../imagem/tuberculosis-phone-0088.xml","xml")
